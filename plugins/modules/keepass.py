1304f6194d557d7e3eb000c8595bf9c524561934
Quico <mister.quico@protonmail.com>
2021-10-26 10:27:09 +0200

    Modify def generate_password of keepass.py

--- plugins/modules/keepass.py
+++ plugins/modules/keepass.py
@@ -239,7 +239,7 @@ def main():
 
 def generate_password(length):
     import string
-    alphabet = string.ascii_letters + string.digits
+    alphabet = string.ascii_letters + string.digits + string.punctuation
     try:
         import secrets as random
     except ImportError:
